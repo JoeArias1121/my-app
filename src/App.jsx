25d0cf48d9e85ae20c0ca2d1b889e6098e127ff1
JoeArias1121 <joearias1121@gmail.com>
2024-01-05 23:57:19 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <Header />
+            </header>
+            <body>
+                <div className="note">
+                    
+                </div>
+            </body>
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
+
+export default App;
